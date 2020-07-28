89fe774586897b3005fa6506e801546057655f4d
woodi3 <wawood@email.sc.edu>
2020-07-28 19:15:37 -0400

    Get album reviews lined up

--- src/components/AlbumReviews.vue
+++ src/components/AlbumReviews.vue
@@ -1,6 +1,46 @@
 <template>
     <zen-box class="album-reviews pt-8 pb-8" v-if="albumReviews">
-        <zen-box class="container">
+        <zen-flex class="container" wrap="wrap">
+            <zen-box f="0 0 50%"
+                bg="accent-light" 
+                class="title-box"
+                pt="8rem"
+                pl="2rem" 
+                responsive>
+                <zen-text color="white" 
+                    header="h1"
+                    lineHeight="1"
+                    transform="uppercase">Album Reviews</zen-text>
+                <zen-button>
+                    View More
+                </zen-button>
+            </zen-box>
+            <zen-box f="0 0 50%" 
+                d="flex" 
+                flexDirection="row"
+                flexWrap="wrap" 
+                responsive>
+                <zen-box f="0 0 50%"
+                    v-for="albumReview in albumReviews"
+                    :key="albumReview._id"
+                    responsive
+                    class="album-review"
+                    position="relative">
+                    <img :src="albumReview.headerImg" />
+                    <zen-box class="album-review-title-container">
+                        <zen-box class="text"
+                            w="100%" 
+                            h="100%"
+                            position="relative">
+                            <zen-text header="h6" class="album-review-title" bold color="black">
+                                {{albumReview.title}}
+                            </zen-text>
+                        </zen-box>
+                    </zen-box>
+                </zen-box>
+            </zen-box>
+        </zen-flex>
+        <!-- <zen-box class="container">
             <zen-text class="uppercase text-center title" 
                 header="h7"
                 letterSpacing="2px">
@@ -31,14 +71,14 @@
                         @click="goToDetail(activeReview)"/>
                 </zen-box>
             </zen-flex>
-        </zen-box>
+        </zen-box> -->
     </zen-box>
 </template>
 
 <script>
-import Divider from './Divider'
-import AlbumReview from './AlbumReview'
-import AlbumReviewSkeleton from './AlbumReviewSkeleton'
+// import Divider from './Divider'
+// import AlbumReview from './AlbumReview'
+// import AlbumReviewSkeleton from './AlbumReviewSkeleton'
 
 export default {
     props: {
@@ -51,9 +91,9 @@ export default {
         },
     },
     components: {
-        Divider,
-        AlbumReviewSkeleton,
-        AlbumReview,
+        // Divider,
+        // AlbumReviewSkeleton,
+        // AlbumReview,
     },
     data () {
         return {
@@ -87,19 +127,37 @@ export default {
 
 <style scoped>
 
-.album-reviews h2 {
-    margin: 0;
+.album-reviews .container {
+    padding: .5rem;
 }
-.album-reviews h4 {
-    margin: 0px;
-    padding-top: calc(var(--spacing-rem) * 4);
+.album-reviews > .container > .title-box {
+    min-height: 600px;
 }
-
-.post-img {
-    width: 100%;
-    max-height: 600px;
+.album-review {
+    height: 300px;
 }
-.post-img:hover {
+.album-review:hover > .album-review-title-container {
+    display: block;
+    top: 0;
+    left: 0;
+    background-color: var(--gray-lighter-color);
+    opacity: .75;
     cursor: pointer;
+}
+.album-review-title-container {
+    display: none;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    
+}
+.album-review-title {
+    position: absolute;
+    bottom: calc(var(--spacing-rem) * 4);
+    left: calc(var(--spacing-rem) * 2);
+}
+.album-review > img {
+    height: 100%;
+    width: 100%;
 }
 </style>
