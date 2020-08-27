fbb532bf14c5be20ef293aac79d2feaa43af6b14
woodi3 <wawood@email.sc.edu>
2020-08-27 18:30:44 -0400

    Fix env variables

--- environment.js
+++ environment.js
@@ -4,4 +4,5 @@ dotenv.config();
 
 export default {
     STRIPE_KEY: process.env.VUE_APP_STRIPE_KEY || 'ENTER YOUR STRIPE KEY',
+    BASE_API_URL: process.env.VUE_APP_STRIPE_KEY || 'http://localhost:3000/v1/',
 };
