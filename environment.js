278544f12180ed255bbb12da88826e253a21ff85
William Wood <woodawilliam@gmail.com>
2020-08-27 22:24:35 -0400

    Merging dev into master (#33)
    * pushing dev branch
    
    * remove change
    
    * Switching to new branch for further changes
    
    * redo desktop nav (#1)
    
    * Added Latest Posts section on home page (#3)
    
    * Trending posts component redo (#4)
    
    * Redo code section on home page (#5)
    
    * Desktop code redo (#6)
    
    * Redo code section on home page
    
    * Layout changes regarding large icon, animations
    
    * Desktop album review redo (#7)
    
    * Get album reviews lined up
    
    * Album review redo
    
    * Add click events to UI redo (#8)
    
    * #9 Fix tab animation on load (#19)
    
    * (closes #10) Add text-black class to privacy policy (#20)
    
    * (closes #11) fix broken code link (#21)
    
    * (closes #13) Clear session if token auth fails, no-cache on server (#22)
    
    Set a no-cache header on the server to actually fix the issue. The ui fix is more of a safeguard.
    
    * (closes #14) Change postTitle accessor (#23)
    
    * Bottom sheet component (#25)
    
    * (linked #12) Create bottom sheet component, implement bookmarks with bottom sheet component
    
    * (linked #12) Implement bottom sheet for change password
    
    * (closes #12) Implement bottom sheet for buy me a coffee
    
    * (closes #15) code-preview mobile friendly, trending-posts mobile friendly, shrink logo on mobile (#26)
    
    * Unit tests (#28)
    
    * (linked #17) setup Jest, add unit tests for few components
    
    * (closes #17) add unit tests
    
    * remove unused components
    
    * (closes #29) change buy coffee img to block button
    
    * (bugfix) remove footer from hiphop and code view, add scope to bookmarks style tag
    
    * Color changes (#31)
    
    * Fix env variables
    
    * package updates
    
    * fix api url

--- environment.js
+++ environment.js
@@ -4,5 +4,5 @@ dotenv.config();
 
 export default {
     STRIPE_KEY: process.env.VUE_APP_STRIPE_KEY || 'ENTER YOUR STRIPE KEY',
-    BASE_API_URL: process.env.VUE_APP_STRIPE_KEY || 'http://localhost:3000/v1/',
+    BASE_API_URL: process.env.VUE_APP_BASE_API_URL || 'http://localhost:3000/v1/',
 };
